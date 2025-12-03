74a7f4a8d1a4454f0c477653ee08c073c4c5e6ad
Nev <105143034+zlarosav@users.noreply.github.com>
2025-12-03 08:33:48 -0500

    Refactor topological sort function and variable names

--- Snippets/Graph/Topo_Sort_DFS.cpp
+++ Snippets/Graph/Topo_Sort_DFS.cpp
@@ -1,8 +1,8 @@
 // O(N + M). 0-indexed. Retorna cualquier toposort válido (no necesariamente lexicográficamente mínima)
-vector<int> top_sort(vector<vector<int>>& adj){
+vector<int> topo_sort(vector<vector<int>>& adj){
     int n = adj.size();
     bool cycle = false;
-    vector<int> sorted, color(n); // 0 = no visitado, 1 = visitando, 2 = terminado
+    vector<int> topo, color(n); // 0 = no visitado, 1 = visitando, 2 = terminado
 
     function<void(int)> dfs = [&](int u){
         color[u] = 1;
@@ -11,13 +11,13 @@ vector<int> top_sort(vector<vector<int>>& adj){
             else if (color[v] == 1) cycle = true; // ciclo detectado
         }
         color[u] = 2;
-        sorted.push_back(u);
+        topo.push_back(u);
     };
 
     for (int i = 0; i < n; i++){
         if (color[i] == 0 && !cycle) dfs(i);
     }
     if (cycle) return {}; // no existe toposort
-    reverse(sorted.begin(), sorted.end());
-    return sorted;
+    reverse(topo.begin(), topo.end());
+    return topo;
 }
